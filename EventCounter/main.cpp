f970a10fa7e933c766a311a8987f0c01170101a0
herron9 <yanghrong@outlook.com>
2016-03-19 12:31:42 -0400

    Initial Commit

---
+++ EventCounter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EventCounter
+//
+//  Created by 杉田真 on 3/19/16.
+//  Copyright © 2016 rr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
